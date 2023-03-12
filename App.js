40592f556a041d18e7baf193cfc23bcef0ce06d7
nbal21388 <91699716+nbal21388@users.noreply.github.com>
2023-03-12 20:54:55 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import Apirequest from "./Apirequest";
+import Displayinfo from "./Displayinfo";
+
+function App() {
+  return (
+    <div>
+      <Apirequest />
+      <Displayinfo />
+    </div>
+  )
+}
+
+export default App;
